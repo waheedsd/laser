cccaa33e31b6287ed0c682ceacf82de1649484ac
waheedsd <syedwaheed9292@gmail.com>
2019-10-31 00:25:34 +0530

    wip page structure with out data

--- public/javascripts/wipdata.js
+++ public/javascripts/wipdata.js
@@ -1,23 +1,120 @@
 var data = [];
 function wip(data){
-    var wipclass = $(".wip");
-        let tdata;
-            for(let key in data){
-                var dlnth = data[key].length-1;
-                for(var i = dlnth; i >= 0; i--){
-                    tdata = data[key][i];
-                    var td = $("<td></td>");
-                    var p = $("<p></p>");
-                    td.append(p);
-                    p.append(li1);
-                    var li1 = $("<li></li>");
-                    p.append(li2);
-                    var li2 = $("<li></li>");
-                    wipclass.append(td);
-                console.log(tdata[i]);
-              }
-            }        
+    // Sheet metal
+    var wipStep = $(".wipStep td").length;
+        for(var i=0; i<wipStep;i++){
+            var td = $("<td></td>");
+            td.append(p);
+            var p = $("<p></p>");
+            var li1 = $("<li></li>");
+            p.append(li1);
+            var li2 = $("<li></li>");
+            p.append(li2);
+            $(".smTable").append(td);
+          }
+    // fabrication
+          var fabStep = $(".fabStep td").length;
+        for(var i=0; i<fabStep;i++){
+            var td = $("<td></td>");
+            td.append(p);
+            var p = $("<p></p>");
+            var li1 = $("<li></li>");
+            p.append(li1);
+            var li2 = $("<li></li>");
+            p.append(li2);
+            $(".fabTable").append(td);
+          }
+    // surface treatment
+    var stStep = $(".stStep td").length;
+    for(var i=0; i<stStep;i++){
+        var td = $("<td></td>");
+        td.append(p);
+        var p = $("<p></p>");
+        var li1 = $("<li></li>");
+        p.append(li1);
+        var li2 = $("<li></li>");
+        p.append(li2);
+        $(".stTable").append(td);
+      }
+      //NPI Assembly
+    var npiStep = $(".npiStep").attr("colspan");
+    for(var i=0; i<npiStep;i++){
+        var td = $("<td></td>");
+        td.append(p);
+        var p = $("<p></p>");
+        var li1 = $("<li></li>");
+        p.append(li1);
+        var li2 = $("<li></li>");
+        p.append(li2);
+        $(".npiTable").append(td);
+      }
+      //gap
+      var npiStep = $(".Step").attr("colspan");
+      console.log(npiStep);
+      for(var i=0; i<npiStep;i++){
+          var td = $("<td></td>").css({"visibility":"hidden", "border":"none"});
+          td.append(p);
+          var p = $("<p></p>");
+          var li1 = $("<li></li>");
+          p.append(li1);
+          var li2 = $("<li></li>");
+          p.append(li2);
+          $(".npiTable").append(td);
+        }
+      //Job Production
+      var npiStep = $(".jobStep").attr("colspan");
+      console.log(npiStep);
+      for(var i=0; i<npiStep;i++){
+          var td = $("<td></td>");
+          td.append(p);
+          var p = $("<p></p>");
+          var li1 = $("<li></li>");
+          p.append(li1);
+          var li2 = $("<li></li>");
+          p.append(li2);
+          $(".npiTable").append(td);
+        }
+         //assembly
+    var assemblyStep = $(".assemblyStep td").length;
+    console.log(assemblyStep);
+    for(var i=0; i<assemblyStep;i++){
+        var td = $("<td></td>");
+        td.append(p);
+        var p = $("<p></p>");
+        var li1 = $("<li></li>");
+        p.append(li1);
+        var li2 = $("<li></li>");
+        p.append(li2);
+        $(".assemblyTable").append(td);
+      }
+      //assemblyGl
+      var assemblyGlStep = $(".assemblyGlStep").attr("colspan");
+      for(var i=0; i<assemblyGlStep;i++){
+          var td = $("<td></td>");
+          td.append(p);
+          var p = $("<p></p>");
+          var li1 = $("<li></li>");
+          p.append(li1);
+          var li2 = $("<li></li>");
+          p.append(li2);
+          $(".assemblyGlTable").append(td);
+        }
+        //AssemblyHl
+        var assemblyHlStep = $(".assemblyHlStep").attr("colspan");
+        for(var i=0; i<assemblyHlStep;i++){
+            var td = $("<td></td>");
+            td.append(p);
+            var p = $("<p></p>");
+            var li1 = $("<li></li>");
+            p.append(li1);
+            var li2 = $("<li></li>");
+            p.append(li2);
+            $(".assemblyHlTable").append(td);
+          }
     }
+   
+    
+
 $(document).ready(function(){
     $.ajax({
         url:"/wip",
@@ -28,10 +125,10 @@ $(document).ready(function(){
                 // for(var key in data){
                 //    let data1 = data[key];
                 //    for(var key1 in data1){
-                       wip(data);
-                //        console.log(data1[key1]);
+                //        wip(data1[key1]);
                 //    }
                 // }
+                wip(data);
         },
         error:function(error){
             console.log(error);
